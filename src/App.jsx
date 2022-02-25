798222982a5866b60cd5d6eb24d239e4675b7c3f
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2022-02-24 16:58:29 -0800

    wrapped app in ProfileProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
+import { ProfileProvider } from './context/ProfileContext';
 import { UserProvider } from './context/UserContext';
 import Auth from './views/Auth/Auth';
 import ConfirmEmail from './views/Auth/ConfirmEmail';
@@ -9,23 +10,25 @@ import Home from './views/Home/Home';
 function App() {
   return (
     <UserProvider>
-      <Router>
-        <Header />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/login">
-            <Auth />
-          </Route>
-          <Route path="/register">
-            <Auth isSigningUp />
-          </Route>
-          <Route path="/confirm-email">
-            <ConfirmEmail />
-          </Route>
-        </Switch>
-      </Router>
+      <ProfileProvider>
+        <Router>
+          <Header />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/login">
+              <Auth />
+            </Route>
+            <Route path="/register">
+              <Auth isSigningUp />
+            </Route>
+            <Route path="/confirm-email">
+              <ConfirmEmail />
+            </Route>
+          </Switch>
+        </Router>
+      </ProfileProvider>
     </UserProvider>
   );
 }
